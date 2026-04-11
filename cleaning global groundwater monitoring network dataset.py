13a9a20026e9d9b42f8c0a86ec5dc0ca3c8e4097
talkwitharnav-web <talkwitharnav@gmail.com>
2026-04-10 19:44:58 -0700

    Limiting scope of global groundwater monitoring network dataset to US only. Generating yp report for that file.

---
+++ cleaning global groundwater monitoring network dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from pathlib import Path
+# Finding working directory!
+BASE_DIR = Path(__file__).parent
+# Defining where the dataset is
+DATA_FILE = BASE_DIR / "Global Groundwater Monitoring Network dataset.csv"
+# Defining where the new dataset should be saved
+OUTPUT_FILE = BASE_DIR / "Clean Datasets" / "Cleaned Global Groundwater Monitoring Network dataset.csv"
+# Finding "country" column in the dataset and filtering to have only "United States".
+# Then save to a new CSV file.
+df = pd.read_csv(DATA_FILE)
+us_df = df[df["country"] == "United States"]
+# Putting clean file in "Clean Datasets" folder and saving as a new CSV file.
+us_df.to_csv(OUTPUT_FILE, index=False)
+print(f"Cleaned file saved to: {OUTPUT_FILE}")
